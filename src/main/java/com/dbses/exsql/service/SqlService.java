1ea516bf1d4df0aea0f96ed3d5840e393ff287a1
yanglulu <yanglu_u@126.com>
2020-08-07 17:39:00 +0800

    增加AUTO INCREMENT

--- src/main/java/com/dbses/exsql/service/SqlService.java
+++ src/main/java/com/dbses/exsql/service/SqlService.java
@@ -13,7 +13,7 @@ public class SqlService {
     private static final String BRACKET_LETF = "(";
     private static final String BRACKET_RIGHT = ")";
     private static final String COMMENT = "COMMENT";
-    //    private static final String AUTO_INCREMENT = "AUTO_INCREMENT";
+    private static final String AUTO_INCREMENT = "AUTO_INCREMENT";
     private static final String EMPTY = " ";
     private static final String QUOTES = "'";
     private static final String COMMA = ",";
@@ -30,19 +30,22 @@ public class SqlService {
         for (Map.Entry<String, List<Table>> entry : tablesMap.entrySet()) {
             String tableName = entry.getKey();
             List<Table> tables = entry.getValue();
+            sql.append("-- 创建").append(tableName.toUpperCase()).append(BREAK_LINE);
             sql.append(CREATE_TABLE).append(EMPTY).append(tableName).append(EMPTY).append(BRACKET_LETF).append(BREAK_LINE);
             for (int i = 0; i < tables.size(); i++) {
                 Table table = tables.get(i);
                 sql.append(table.getFieldName()).append(EMPTY)
-                        .append(table.getDataType()).append(EMPTY)
-                        .append(COMMENT).append(EMPTY).append(QUOTES).append(table.getChineseName()).append(QUOTES);
+                        .append(table.getDataType()).append(EMPTY);
+                if (table.isAutoIncrease()) {
+                    sql.append(AUTO_INCREMENT).append(EMPTY);
+                }
+                sql.append(COMMENT).append(EMPTY).append(QUOTES).append(table.getChineseName()).append(QUOTES);
                 if (i != tables.size() - 1) {
                     sql.append(COMMA);
                 }
                 sql.append(BREAK_LINE);
             }
             sql.append(BRACKET_RIGHT).append(ENGINE).append(SEMICOLON).append(BREAK_LINE);
-            sql.append("-----------------------------------------------").append(BREAK_LINE);
         }
         return sql.toString();
     }
